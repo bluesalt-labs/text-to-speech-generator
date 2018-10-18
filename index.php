3bd1aaff498d74a1f6b6397bca4d14f6415517a8
Luke Sontrop <luke@bluesaltlabs.com>
2018-10-18 12:31:10 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TextToSpeech Generator</title>
+
+    <link rel="stylesheet" href="/resources/css/app.css" />
+</head>
+<body>
+    
+</body>
+
+<script type="text/javascript" src="/resources/js/app.js"></script>
+</html>
---
+++ resources/views/form.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luke
+ * Date: 10/18/18
+ * Time: 12:27 PM
+ */
